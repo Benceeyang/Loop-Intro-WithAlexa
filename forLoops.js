43df0dc48ce69026054b897e167d2b9b10c41435
Bencee Yang <byang@icstars.org>
2022-07-22 10:55:27 -0500

    update

---
+++ forLoops.js
@@ -0,0 +1,18 @@
+//For loop is the most common loop that's used and tpyically used to run a chunk of code a certain amount of time
+
+
+
+//Syntax
+// for (initializer; Condition; iteration) {
+    //chunk code
+// }
+
+//NOTE Example
+//Initilizer:
+//Condition:
+//Iteration:
+
+for (let i = 0; i <= 5; i++){
+    //chunk code
+    console.log(i);
+}
---
+++ whileLoops.js
@@ -0,0 +1,40 @@
+//initializer: starting value
+//consition: "run this code if true" (return true/false)
+//iteration: adds on to the initial value to continure the loop
+
+//NOTE Description: the most simple looping statement in Javascript! Loops through a chunck of code as long as the condition is true
+
+//TODO 
+
+//FIXME
+
+// //NOTE Syntax
+// let i = 0; //Initializer
+
+// while (condition){
+//     //chunk of code that occurs
+//     i++; //iteration - post-increment: 0 , 1
+//     ++i; // pre-increment: 1 , 2
+// }
+
+//NOTE post-increments: i++; :  0 , 1 | pre-increment: ++i; : 1 , 2
+
+//NOTE example:
+// True / Success
+// let j = 0;
+
+// while (j < 5){
+//     console.log(j);
+//     ++j;
+//     console.log("Current val:" + j)
+// }
+
+//False / Fail
+//False because the condition was never true!
+// let k = 0 //initializer
+
+// while (k > 5){
+//     console.log(k);
+//     k++;
+// }
+
