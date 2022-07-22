331ac246f902da37c6f6ac806a72a50365e289bb
Bencee Yang <byang@icstars.org>
2022-07-22 11:07:25 -0500

    update

--- forLoops.js
+++ forLoops.js
@@ -11,8 +11,13 @@
 //Initilizer:
 //Condition:
 //Iteration:
+//Simple 0 - 5 loop
+// for (let i = 0; i <= 5; i++){
+//     //chunk code
+//     console.log(i);
+// }
 
-for (let i = 0; i <= 5; i++){
-    //chunk code
-    console.log(i);
-}
+let avengers = ["Thor", "CptAmerica", "IronMan", "Hulk", "HawkEye"];
+    for (let a = 0; a < avengers.length; a++){
+        console.log("You chose " + avengers[a])
+};
