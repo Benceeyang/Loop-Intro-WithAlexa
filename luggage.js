9ccdcdca2c1d5a992a7ef4ee8fae0e48457da29b
Bencee Yang <byang@icstars.org>
2022-07-22 12:19:33 -0500

    update

---
+++ luggage.js
@@ -0,0 +1,10 @@
+/* 
+1) Luggage is loaded into containers as it arrives
+2) When the weight is full (MAX 40lbs), the container is loaded into the aircraft
+3) At the destination, th containers are then unloaded from the plane LIFO (last in, first out)
+4) Finally, the luggage is handed back individually
+
+Example: 
+Input: [30,5,6]
+Key Point #1
+*/
